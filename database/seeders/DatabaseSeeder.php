c41d1bc778c0e4a19900caeb9d198f037d2a7a8f
sandeep8558 <sandeep198558@gmail.com>
2026-07-21 14:08:14 +0530

    Use firstOrCreate in DatabaseSeeder to safely create users if they do not exist

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,43 +19,62 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // Seed Sandeep Rathod with all roles
-        User::factory()->create([
-            'name' => 'Sandeep Rathod',
-            'email' => 'sandeep198558@yahoo.com',
-            'mobile_number' => '9664588677',
-            'roles' => ['admin', 'manager', 'driver', 'user'],
-            'password' => Hash::make('password'),
-            'latitude' => 12.971598,
-            'longitude' => 77.594566,
-        ]);
+        // Seed Sandeep Rathod if doesn't exist
+        User::firstOrCreate(
+            ['email' => 'sandeep198558@gmail.com'],
+            [
+                'name' => 'Sandeep Rathod',
+                'mobile_number' => '9664588677',
+                'roles' => ['admin', 'manager', 'driver', 'user'],
+                'password' => Hash::make('password'),
+                'latitude' => 12.971598,
+                'longitude' => 77.594566,
+            ]
+        );
 
-        // Seed Stop locations around Sandeep's location in Bengaluru
-        Stop::create([
-            'name' => 'Sarvodaya Nagar Main Gate',
-            'type' => 'auto',
-            'city' => 'Ambernath',
-            'latitude' => 19.189387605307818,
-            'longitude' => 73.21953875219381,
-            'status' => 'active',
-        ]);
+        // Seed Leena Adam if doesn't exist
+        User::firstOrCreate(
+            ['email' => 'leenaadam28@gmail.com'],
+            [
+                'name' => 'Leena Adam',
+                'mobile_number' => '9769409405',
+                'roles' => ['admin', 'manager', 'user'],
+                'password' => Hash::make('password'),
+            ]
+        );
 
-        Stop::create([
-            'name' => 'Sarvodaya Nagar, Phase 3',
-            'type' => 'auto',
-            'city' => 'Ambernath',
-            'latitude' => 19.188054600497303,
-            'longitude' => 73.2208181397075,
-            'status' => 'active',
-        ]);
+        // Seed Stop locations if they don't exist
+        Stop::firstOrCreate(
+            ['name' => 'Sarvodaya Nagar Main Gate'],
+            [
+                'type' => 'auto',
+                'city' => 'Ambernath',
+                'latitude' => 19.189387605307818,
+                'longitude' => 73.21953875219381,
+                'status' => 'active',
+            ]
+        );
 
-        Stop::create([
-            'name' => 'Orchid, Sarvodaya Nagar',
-            'type' => 'auto',
-            'city' => 'Ambernath',
-            'latitude' => 19.189818686453105,
-            'longitude' => 73.2207734974165,
-            'status' => 'active',
-        ]);
+        Stop::firstOrCreate(
+            ['name' => 'Sarvodaya Nagar, Phase 3'],
+            [
+                'type' => 'auto',
+                'city' => 'Ambernath',
+                'latitude' => 19.188054600497303,
+                'longitude' => 73.2208181397075,
+                'status' => 'active',
+            ]
+        );
+
+        Stop::firstOrCreate(
+            ['name' => 'Orchid, Sarvodaya Nagar'],
+            [
+                'type' => 'auto',
+                'city' => 'Ambernath',
+                'latitude' => 19.189818686453105,
+                'longitude' => 73.2207734974165,
+                'status' => 'active',
+            ]
+        );
     }
 }
